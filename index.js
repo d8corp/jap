9cfb740a92dba6d6a51c930ab5f7dffbe92b85f5
Mike <d8corp@mail.ru>
2019-06-05 23:29:12 +0300

    refactored jap functionality

--- index.js
+++ index.js
@@ -4,34 +4,37 @@ var typeFilter = require('type-filter/typeFilter');
 var yes = require('type-filter/handlers/yes');
 
 /**
- * @param {*} [request]
  * @param {*} [handler]
+ * @param {*} [request]
  * @param {function(value)} [resolve]
- * @param {function(request,handler,error?)} [reject]
+ * @param {function(request,error:Error,handler)} [reject]
  * @param {Array<Promise>} [promises]
  * @param {Object} [options]
  * */
-function jap (request, handler, resolve, reject, promises, options) {
+function jap (handler, request, resolve, reject, promises, options) {
   if (!resolve) {
     resolve = yes
   }
   if (!reject) {
     reject = yes
   }
+  var resolveHandler = function () {
+    return resolve(handler);
+  };
   return typeFilter(handler, {
     object: function (handler, options) {
       return typeFilter(request, {
         array: function (req) {
           const length = req.length;
           for (let i = 0; i < length; i++) {
-            jap(req[i], handler, resolve, reject, promises, options);
+            jap(handler, req[i], resolve, reject, promises, options);
           }
           return request
         },
         object: function (req) {
           for (const key in req) {
             if (key in Object.prototype) continue;
-            var result = jap(req[key], handler[key], resolve, reject, promises);
+            var result = jap(handler[key], req[key], resolve, reject, promises);
             if (result && result.then) {
               result.then(function (v) {
                 return req[key] = v
@@ -45,7 +48,7 @@ function jap (request, handler, resolve, reject, promises, options) {
           return request
         },
         other: function () {
-          return reject(request, handler);
+          return reject(request, Error('Undeclared request'), handler);
         }
       })
     },
@@ -55,7 +58,7 @@ function jap (request, handler, resolve, reject, promises, options) {
           try {
             return resolve(handler.apply(handler, request))
           } catch (e) {
-            return reject(request, handler, e)
+            return reject(request, e, handler)
           }
         },
         other: function () {
@@ -65,7 +68,7 @@ function jap (request, handler, resolve, reject, promises, options) {
               result = result.then(function (v) {
                 return resolve(v);
               }, function (e) {
-                return reject(request, handler, e);
+                return reject(request, e, handler);
               });
               if (promises) {
                 promises.push(result)
@@ -75,18 +78,29 @@ function jap (request, handler, resolve, reject, promises, options) {
               return resolve(result)
             }
           } catch (e) {
-            return reject(request, handler, e)
+            return reject(request, e, handler)
           }
         }
       })
     },
     array: function () {
-      return handler.reduce(function (request, handler) {
-        return jap(request, handler, resolve, reject, promises)
-      }, request)
+      const length = handler.length;
+      let i = 0;
+      try {
+        while (i < length) {
+          request = jap(handler[i++], request, resolve, reject, promises)
+        }
+      } catch (e) {
+        return reject(request, e, handler[i - 1]);
+      }
+      return request;
     },
+    string: resolveHandler,
+    number: resolveHandler,
+    boolean: resolveHandler,
+    null: resolveHandler,
     other: function () {
-      return reject(request, handler);
+      return reject(request, Error('Missed handler'), handler);
     }
   }, options);
 }
--- index.test.js
+++ index.test.js
@@ -2,319 +2,80 @@
 const jap = require('./index');
 
 describe('jap', () => {
-  describe('command', () => {
-    it('simple', () => {
-      expect(jap()).toBe(undefined);
-    });
-    it('any', () => {
-      expect(jap(1)).toBe(1);
-      expect(jap('1')).toBe('1');
+  describe('resolve', () => {
+    it('handler is a primitive', () => {
       expect(jap(null)).toBe(null);
       expect(jap(true)).toBe(true);
-      expect(jap([])).toEqual([]);
-      expect(jap({})).toEqual({});
-      expect(jap({test: true})).toEqual({test: true});
+      expect(jap(1)).toBe(1);
+      expect(jap(2.5)).toBe(2.5);
+      expect(jap('v1.0.0')).toBe('v1.0.0');
     });
-  });
-  describe('handler', () => {
-    it('function', () => {
-      expect(jap(3, x => x * x)).toBe(9);
+    it('handler is a function', () => {
+      expect(jap(() => 1)).toEqual(1);
+      expect(jap(x => x * x, 3)).toBe(9);
+      expect(jap(x => `x: ${x}`, 'test')).toBe('x: test');
+      expect(jap(x => !x, true)).toBe(false);
+      expect(jap(x => x(), () => 'test')).toBe('test');
     });
-    it('spread', () => {
-      expect(jap([3, 5], (x, y) => x + y)).toBe(8);
+    it('handler is an array', () => {
+      expect(jap([1])).toEqual(1);
+      expect(jap([1, 2])).toEqual(2);
+      expect(jap([3, x => x * x])).toEqual(9);
+      expect(jap([x => x * x, x => x + x], 3)).toEqual(18);
+      expect(jap([x => x * x, [x => x + x]], 3)).toEqual(18);
     });
-    describe('array', () => {
-      it('empty', () => {
-        expect(jap(3, [])).toBe(3);
-      });
-      it('function', () => {
-        const square = x => x * x;
-        const addOne = x => x + 1;
-        expect(jap(3, [square, addOne])).toBe(10);
-      });
-      it('array', () => {
-        const square = x => x * x;
-        const addOne = x => x + 1;
-        expect(jap(3, [square, [addOne, [addOne, addOne]]])).toBe(12);
-      });
-      it('object', () => {
-        const addOne = x => x + 1;
-        expect(jap(3, [{}, addOne])).toBe(4);
-      });
-      it('command object', () => {
-        const addOne = x => x + 1;
-        expect(jap({test: 1}, [{test: addOne}, {test: addOne}])).toEqual({test: 3});
-      });
-      it('any', () => {
-        const addOne = x => x + 1;
-        expect(jap(1, [addOne, 1, '2', false, null, undefined, new Map()])).toBe(2);
-      });
+    it('handler is an object', () => {
+      const square = x => x * x;
+      const double = x => x + x;
+      expect(jap({test: 1}, {test: null})).toEqual({test: 1});
+      expect(jap({square}, {square: 3})).toEqual({square: 9});
+      expect(jap({settings: {version: '1.0.0'}}, {settings: {version: null}})).toEqual({settings: {version: '1.0.0'}});
+      expect(jap({double: [x => x | 0, double]}, {double: '3'})).toEqual({double: 6});
+      expect(jap({double: [x => x | 0, double], square}, {double: '4', square: '3'})).toEqual({double: 8, square: 9});
+      const addOne = x => x + 1;
+      expect(jap([{test: addOne}, {test: addOne}], {test: 3})).toEqual({test: 5});
+      expect(jap([{test: addOne}, {test: addOne}, 1], {test: 3})).toEqual(1);
     });
-    it('object', () => {
-      const handler = {
-        square: x => x * x
-      };
-      const request = {
-        square: 3
-      };
-      const response = {
-        square: 9
-      };
-      expect(jap(request, handler)).toEqual(response);
+    it('request spreading', () => {
+      expect(jap(1, [])).toBe(1);
+      expect(jap((x, y) => x + y, [1, 2])).toBe(3);
+      expect(jap({sum: (x, y) => x + y}, {sum: [1, 2]})).toEqual({sum: 3});
+      expect(jap({sum: [
+        (...a) => a.map(x => x | 0),
+        (x, y) => x + y
+      ]}, {sum: ['1', '2']})).toEqual({sum: 3});
+      expect(jap([x => [x | 0, x % 1 * 100 | 0], (ceil, decimal) => ({ceil, decimal})], 3.141)).toEqual({ceil: 3, decimal: 14});
     });
-    it('arguments', () => {
-      const handler = {
-        sum: (x, y) => x + y
-      };
-
-      const request = {
-        sum: [3, 6]
-      };
-      const response = {
-        sum: 9
-      };
-
-      expect(jap(request, handler)).toEqual(response);
-    });
-    it('nesting', () => {
-      const handler = {
-        math: {
-          sum: (x, y) => x + y
-        }
-      };
-
-      const request = {
-        math: {
-          sum: [3, 6]
-        }
-      };
-      const response = {
-        math: {
-          sum: 9
-        }
-      };
-
-      expect(jap(request, handler)).toEqual(response);
-    });
-    it('deep nesting', () => {
-      const handler = {
-        calc: {
-          math: {
-            sum: (x, y) => x + y
-          }
-        }
-      };
-
-      const request = {
-        calc: {
-          math: {
-            sum: [3, 6]
-          }
-        }
-      };
-      const response = {
-        calc: {
-          math: {
-            sum: 9
-          }
-        }
-      };
-
-      expect(jap(request, handler)).toEqual(response);
-    });
-    it('combine', () => {
-      const handler = {
-        math: {
-          max: (...args) => Math.max(...args),
-          min: (...args) => Math.min(...args)
-        }
-      };
-
-      const request = {
-        math: {
-          min: [3, 6, 1, 12, 5, 22, 8],
-          max: [3, 6, 1, 12, 5, 22, 8]
-        }
-      };
-      const response = {
-        math: {
-          min: 1,
-          max: 22
-        }
-      };
-
-      expect(jap(request, handler)).toEqual(response);
-    });
-    it('multiple', () => {
-      const handler = {
-        minmax: (...args) => ({
-          min: Math.min(...args),
-          max: Math.max(...args)
-        })
-      };
-
-      const request = [
-        {
-          minmax: [3, 6, 1, 12, 5, 22, 8]
-        }, {
-          minmax: [-1, 6, 7, 12, 5, -2, 8]
-        }
-      ];
-      const response = [
-        {
-          minmax: {
-            min: 1,
-            max: 22
-          }
-        }, {
-          minmax: {
-            min: -2,
-            max: 12
-          }
-        }
-      ];
-      expect(jap(request, handler)).toEqual(response);
-    });
-    it('deep multiple', () => {
-      const handler = {
-        test: {
-          minmax: (...args) => ({
-            min: Math.min(...args),
-            max: Math.max(...args)
-          })
-        }
-      };
-
-      const request = {
-        test: [
-          {
-            minmax: [3, 6, 1, 12, 5, 22, 8]
-          }, {
-            minmax: [-1, 6, 7, 12, 5, -2, 8]
-          }
-        ]
-      };
-      const response = {test: [
-        {
-          minmax: {
-            min: 1,
-            max: 22
-          }
-        }, {
-          minmax: {
-            min: -2,
-            max: 12
-          }
-        }
-      ]};
-      expect(jap(request, handler)).toEqual(response);
-    });
-  });
-  describe('resolve', () => {
-    it('function', () => {
+    it('resolve argument', () => {
       const resolve = value => ({error: false, value});
-      expect(jap(1, e => e + 1, resolve)).toEqual({
-        error: false,
-        value: 2
-      });
-    });
-    it('object', () => {
-      const resolve = value => ({error: false, value});
-      expect(jap({test: 1}, {test: e => e + 1}, resolve)).toEqual({
-        test: {
-          error: false,
-          value: 2
-        }
-      });
+      expect(jap(1, [], resolve)).toEqual({error: false, value: 1});
+      expect(jap({sum: (x, y) => x + y}, {sum: [1, 2]}, resolve)).toEqual({sum: {error: false, value: 3}});
     });
   });
   describe('reject', () => {
-    it('handler is undefined', () => {
-      const resolve = value => ({error: false, value});
-      const reject = (command, handler, error) => ({error: error || true, command, handler});
-
-      const handler = {
-        test1: e => e
-      };
-      expect(jap({
-        test1: 1,
-        test2: 2
-      }, handler, resolve, reject)).toEqual({
-        test1: {
-          error: false,
-          value: 1
-        },
-        test2: {
-          error: true,
-          command: 2,
-          handler: undefined
-        }
-      });
-      expect(jap({
-        test1: {
-          test2: 1
-        }
-      }, handler, resolve, reject)).toEqual({
-        test1: {
-          error: false,
-          value: {
-            test2: 1
-          }
-        }
-      });
-      expect(jap({
-        test2: {
-          test1: 1
-        }
-      }, handler, resolve, reject)).toEqual({
-        test2: {
-          error: true,
-          handler: undefined,
-          command: {
-            test1: 1
-          }
-        }
-      });
+    it('undeclared handler', () => {
+      expect(jap()).toBe(undefined);
+      expect(jap(undefined)).toBe(undefined);
+      expect(jap(Symbol())).toBe(undefined);
+      expect(jap(new Map())).toBe(undefined);
+      expect(jap(new Set())).toBe(undefined);
+      expect(jap(Error('test'))).toBe(undefined);
+      expect(jap({})).toEqual(undefined);
+      expect(jap([])).toEqual(undefined);
     });
-    it('handler is not function', () => {
-      const resolve = value => ({error: false, value});
-      const reject = (command, handler, error) => ({error: error || true, command, handler});
-
-      const handler = {
-        test: 'test3 value'
-      };
-      expect(jap({
-        test: 1
-      }, handler, resolve, reject)).toEqual({
-        test: {
-          error: true,
-          command: 1,
-          handler: 'test3 value'
-        }
-      });
+    it('wrong nesting', () => {
+      expect(jap({settings: {version: '1.0.0'}}, {settings: null})).toEqual({settings: null});
     });
-    it('an error inside a handler', () => {
-      const resolve = value => ({error: false, value});
-      const reject = (command, handler, error) => ({error: error || true, command, handler});
-
-      const handler = {
-        test: () => {throw Error('test4 error')}
-      };
-      expect(jap({
-        test: 1
-      }, handler, resolve, reject)).toEqual({
-        test: {
-          error: Error('test4 error'),
-          command: 1,
-          handler: handler.test
-        }
-      });
+    it('reject argument', () => {
+      const reject = (request, error, handler) => ({error: error || true, request, handler});
+      const settings = {version: '1.0.0'};
+      expect(jap({settings}, {settings: null}, undefined, reject)).toEqual({settings: {error: Error('Undeclared request'), request: null, handler: settings}});
     });
   });
   describe('async', () => {
     it('function', async () => {
       const resolve = value => ({error: false, value});
-      const reject = (command, handler, error) => ({error: error || true, command, handler});
+      const reject = (request, error, handler) => ({request, error, handler});
 
       const handler1 = async e => {
         await new Promise(resolve => setTimeout(resolve, 10));
@@ -325,8 +86,8 @@ describe('jap', () => {
         throw Error('test')
       };
 
-      const result1 = await jap(1, handler1, resolve, reject);
-      const result2 = await jap(1, handler2, resolve, reject);
+      const result1 = await jap(handler1, 1, resolve, reject);
+      const result2 = await jap(handler2, 1, resolve, reject);
 
       expect(result1).toEqual({
         error: false,
@@ -335,12 +96,12 @@ describe('jap', () => {
       expect(result2).toEqual({
         error: Error('test'),
         handler: handler2,
-        command: 1
+        request: 1
       });
     });
     it('object', async () => {
       const resolve = value => ({error: false, value});
-      const reject = (command, handler, error) => ({error: error || true, command, handler});
+      const reject = (request, error, handler) => ({request, error, handler});
 
       const handler = {
         test1: async e => {
@@ -354,7 +115,7 @@ describe('jap', () => {
 
       const promises = [];
 
-      const result = jap({test1: 1, test2: 2}, handler, resolve, reject, promises);
+      const result = jap(handler, {test1: 1, test2: 2}, resolve, reject, promises);
 
       expect(promises.length).toBe(2);
 
@@ -367,7 +128,7 @@ describe('jap', () => {
         },
         test2: {
           error: Error('test'),
-          command: 2,
+          request: 2,
           handler: handler.test2
         }
       });
